717a1c2f2c70d93c0f2004e9fde644cf79f016d9
Picoquio <84530694+Picoquio@users.noreply.github.com>
2022-03-13 23:58:19 -0300

    .

--- repuestos.js
+++ repuestos.js
@@ -15,15 +15,15 @@ class Repuesto {
 }
 
 // creamos los repuestos
-const repuesto1 = new Repuesto('Pastillas de freno', 200, 'https://http2.mlstatic.com/D_NQ_NP_701407-MLA32496072407_102019-V.webp')
-const repuesto2 = new Repuesto('Bujías', 400, 'https://http2.mlstatic.com/D_NQ_NP_642379-MLA40835465698_022020-V.webp')
-const repuesto3 = new Repuesto('Neumático', 1200, 'https://http2.mlstatic.com/D_NQ_NP_638692-MLA32238541281_092019-V.webp')
-const repuesto4 = new Repuesto('Bomba de nafta', 700, 'https://http2.mlstatic.com/D_NQ_NP_689035-MLA31041134127_062019-V.webp')
-const repuesto5 = new Repuesto('Liquido refrigerante', 180, 'https://http2.mlstatic.com/D_NQ_NP_680624-MLA47074917793_082021-V.webp')
-const repuesto6 = new Repuesto('Liquido para transmisión', 180, 'https://http2.mlstatic.com/D_NQ_NP_972116-MLA44923286875_022021-V.webp')
-const repuesto7 = new Repuesto('Aceite lubricante', 180, 'https://http2.mlstatic.com/D_NQ_NP_837617-MLA41501044111_042020-V.webp')
-const repuesto8 = new Repuesto('Espejo retrovisor', 200, 'https://http2.mlstatic.com/D_NQ_NP_900507-MLA42142836684_062020-V.webp')
-const repuesto9 = new Repuesto('Ópticas faro led', 500, 'https://http2.mlstatic.com/D_NQ_NP_773730-MLA48858915772_012022-V.webp')
+const repuesto1 = new Repuesto('Pastillas de freno', 200, './media/1.jpg')
+const repuesto2 = new Repuesto('Bujías', 400, './media/bujias.jpg')
+const repuesto3 = new Repuesto('Neumático', 1200, './media/ruedas.jpg')
+const repuesto4 = new Repuesto('Bomba de nafta', 700, './media/bombaNafta.jpg')
+const repuesto5 = new Repuesto('Liquido refrigerante', 180, './media/refrigerante.jpg')
+const repuesto6 = new Repuesto('Liquido para transmisión', 180, './media/liquidoTransmision.jpg')
+const repuesto7 = new Repuesto('Aceite lubricante', 180, './media/aceite.jpg')
+const repuesto8 = new Repuesto('Espejo retrovisor', 200, './media/espejo.jpg')
+const repuesto9 = new Repuesto('Ópticas faro led', 500, './media/faros.jpg')
 
 //Array donde iran los objetos "repuesto"
 const stockRepuestos = [];
@@ -34,6 +34,7 @@ pushToArray(stockRepuestos, repuesto1, repuesto2, repuesto3, repuesto4, repuesto
 
 //anclaje a DOM
 const cardsRepuestos = document.getElementById('cardsRepuestos');
+const alCarrito = document.querySelectorAll('.alCarrito')
 
 for (let i = 0; i < stockRepuestos.length; i++) {
     cardsRepuestos.innerHTML += `
@@ -42,11 +43,31 @@ for (let i = 0; i < stockRepuestos.length; i++) {
         <div class="card-body">
             <h5 class="card-title">${stockRepuestos[i].nombre}</h5>
             <p class="card-text">Precio: ${stockRepuestos[i].precio}</p>
-            <a href="#" class="btn btn-primary">Agregar al carrito</a>
+            <a class="btn btn-primary alCarrito">Agregar al carrito</a>
         </div>
      </div>
     ` 
 }
+
+document.querySelectorAll('.alCarrito').forEach(item => {
+    item.addEventListener('click', () => {
+      //handle click 
+      Toastify({
+        text: "Agregado al carrito :)",
+        duration: 4000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+        onClick: function(){} // Callback after click
+      }).showToast();
+    })
+  })
 
 
 
--- venta.js
+++ venta.js
@@ -1,8 +1,26 @@
 //Anclajes a DOM
 const formVenta = document.getElementById('formVenta')
 
+
 //Eventos
 formVenta.addEventListener('submit', (e) => {
     e.preventDefault();
-    alert('jsjsjsj')
+
+    let marca = document.getElementById('marca').value
+    let modelo = document.getElementById('modelo').value;
+    let fabricacion = document.getElementById('fabricacion').value
+    let precio = document.getElementById('precio').value
+
+    
+    
+    
+    
+    
+    
+    swal({
+        title: "Recibido!", 
+        text: `Nos ha ofrecido un ${marca} ${modelo} del año ${fabricacion}. En breve lo contactaremos. `, 
+        icon: "success"
+    })
+    //formVenta.reset()
 })
